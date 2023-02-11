60533ec41376ed1d5b11c51837d57d7c2a9371c4
Óscar Morales Vivó <oscarmv@mac.com>
2023-02-11 13:41:35 -0800

    Readied test UI

---
+++ PDFPagePickerTestApp/PDFPagePickerTestApp/PDFPagePickerTestAppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  PDFPagePickerTestApp
+//
+//  Created by Óscar Morales Vivó on 2/7/23.
+//
+
+import Cocoa
+import PDFPagePicker
+
+@main
+class PDFPagePickerTestAppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+}
